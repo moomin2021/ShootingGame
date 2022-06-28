1897b7aefff07dcd6a6f5123b9566acabbd9fda8
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-06-28 10:21:53 +0900

    完了

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -13,33 +13,38 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // プレイヤーのワールド座標を取得
-        Vector3 pos = transform.position;
 
-        // 右矢印キーが入力されたとき
-        if (Input.GetKey(KeyCode.RightArrow))
+        // --プレイヤーの移動処理-- //
         {
-            pos.x += 0.01f;
-        }
 
-        // 左矢印キーが入力されたとき
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            pos.x -= 0.01f;
-        }
+            // --プレイヤーのワールド座標を取得
+            Vector3 pos = transform.position;
 
-        // 上矢印キーが入力されたとき
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            pos.z += 0.01f;
-        }
+            // --右矢印キーが入力されたとき
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                pos.x += 0.01f;
+            }
 
-        // 下矢印キーが入力されたとき
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            pos.z -= 0.01f;
-        }
+            // --左矢印キーが入力されたとき
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                pos.x -= 0.01f;
+            }
 
-        transform.position = new Vector3(pos.x, pos.y, pos.z);
+            // --上矢印キーが入力されたとき
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                pos.z += 0.01f;
+            }
+
+            // --下矢印キーが入力されたとき
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                pos.z -= 0.01f;
+            }
+
+            transform.position = new Vector3(pos.x, pos.y, pos.z);
+        }
     }
 }
