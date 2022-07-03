bb2c98225d1052751a141ac4cd15457ad91cc1b1
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-07-04 03:34:39 +0900

    完了

---
+++ Assets/EnemyFixedShot.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFixedShot : MonoBehaviour
+{
+    // --プレイヤー-- //
+    private GameObject player;
+
+    // --弾のゲームオブジェクトを入れる-- //
+    public GameObject bullet;
+
+    // --1回で打ちだす弾の数を決める-- //
+    public int bulletWayNum = 3;
+
+    // --打ちだす弾の間隔を調整する-- //
+    public float bulletWaySpace = 30.0f;
+
+    // --打ち出す弾の角度を調整する-- //
+    public int bulletWayAxis = 0;
+
+    // --打ちだす間隔を決める-- //
+    public float time = 1.0f;
+
+    // --最初に打ち出すまでの時間を決める-- //
+    public float delayTime = 1.0f;
+
+    // --現在のタイマー時間-- //
+    float nowtime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // --タイマーを初期化-- //
+        nowtime = delayTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // --もしプレイヤーの情報が入ってなかったら-- //
+        if (player == null)
+        {
+            // --プロジェクトのPlayerを探して情報を取得する-- //
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        // --タイマーを減らす-- //
+        nowtime -= Time.deltaTime;
+
+        // --もしタイマーが0以下になったら-- //
+        if (nowtime <= 0)
+        {
+            // --角度調整用の変数
+            float bulletWaySpaceSplit = 0.0f;
+
+            // --一回で発射する弾数分だけループする-- //
+            for (int i = 0; i < bulletWayNum; i++)
+            {
+                // --弾を生成
+                CreateShotObject(bulletWaySpace - bulletWaySpaceSplit + bulletWayAxis - transform.localEulerAngles.y);
+
+                // --角度を調整する
+                bulletWaySpaceSplit += (bulletWaySpace / (bulletWayNum - 1)) * 2;
+            }
+
+            // --タイマーを初期化
+            nowtime = time;
+        }
+    }
+
+    private void CreateShotObject(float axis)
+    {
+        // --弾を生成する-- //
+        GameObject bulletClone = Instantiate(bullet, transform.position, Quaternion.identity);
+
+        // --EnemyBulletのゲットコンポーネントを変数として保存-- //
+        var bulletObject = bulletClone.GetComponent<EnemyBullet>();
+
+        // --弾を打ち出したオブジェクトの情報を渡す-- //
+        bulletObject.SetCharacterObject(gameObject);
+
+        // --弾を打ち出す角度を変更する-- //
+        bulletObject.SetForwardAxis(Quaternion.AngleAxis(axis, Vector3.up));
+    }
+}
